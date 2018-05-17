ca07be8bb5c9d4e68a5c4af376cf772f496348e5
fxc0719 <1@fxcdev.com>
2018-05-17 10:21:34 +0800

    finish

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ image_dir_path = os.path.abspath(os.path.dirname(__file__))
 image_dir_path = os.path.join(image_dir_path, 'images')
 image_file_list = os.listdir(image_dir_path)
 
+current_count = 1
+
 for image in image_file_list:
     image_path = os.path.join(image_dir_path, image)
     char_dict = ocr.get_dict(image_path)
     result_list = read.get_text_rect(char_dict)
     file_path = os.path.abspath(os.path.dirname(__file__))
-    file_path = os.path.join(file_path, 'ss.txt')
+    file_path = os.path.join(file_path, 'outputs')
+    file_path = os.path.join(file_path, '%s.txt' % image)
     for result in result_list:
         write.write(file_path, result)
+    print('No.%d. %s finish.' % (current_count, image))
+    current_count += 1
