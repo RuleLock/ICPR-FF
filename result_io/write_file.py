130599e783bf2d2aa5e913f5cddb8657d9ceb7fc
fxc0719 <1@fxcdev.com>
2018-05-17 11:15:23 +0800

    update write

--- result_io/write_file.py
+++ result_io/write_file.py
@@ -1,6 +1,8 @@
 import codecs
+
+
 def write(file_path, text):
     # text = text.decode('utf-8')
-    file = codecs.open(file_path, mode='a', encoding='utf-8')
+    file = open(file_path, mode='a+', encoding='utf-8')
     file.write(text)
     file.write('\n')
